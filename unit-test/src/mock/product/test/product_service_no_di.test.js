f60b5f2efc2758a45443b03c188190fea131c1a8
Dongja <jdh981118@naver.com>
2023-07-30 16:57:26 +0900

    :white_check_mark: Test: bad product mock test

---
+++ unit-test/src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,26 @@
+const ProductService = require('../product_service_no_di');
+const ProductClient = require('../product_client');
+jest.mock('../product_client');
+
+describe('product_service', () => {
+  const fetchItems = jest.fn(async () => [
+    { item: 'Milk', available: true },
+    { item: 'banana', available: false },
+  ]);
+  ProductClient.mockImplementation(() => {
+    return {
+      fetchItems,
+    };
+  });
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+  });
+
+  it('should filter out only available items', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items.length).toBe(1);
+    expect(items).toEqual([{ item: 'Milk', available: true }]);
+  });
+});
