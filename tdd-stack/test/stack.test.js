d07585fea4162e1fe66ed3bd22abb3ad7372a09c
Dongja <jdh981118@naver.com>
2023-08-01 11:29:43 +0900

    Test: add initial stack pop fail test

--- tdd-stack/test/stack.test.js
+++ tdd-stack/test/stack.test.js
@@ -7,7 +7,21 @@ describe('stack', () => {
     stack = new Stack();
   });
 
-  it('initial stack is empty', () => {
-    expect(stack.size).toBe(0);
+  describe('initial stack', () => {
+    it('initial stack is empty', () => {
+      expect(stack.size).toBe(0);
+    });
+
+    it('should throw error if try stack pop when stack is empty', () => {
+      expect(() => {
+        stack.pop();
+      }).toThrow('stack is empty');
+    });
+  });
+
+  it('push', () => {
+    stack.push(1);
+    expect(stack.size).toBe(1);
+    expect(stack.top).toBe(1);
   });
 });
