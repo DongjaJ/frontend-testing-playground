9ee5b758d81d1a5e6a05b92ae6a3d5a2ea30b7a4
Dongja <jdh981118@naver.com>
2023-08-01 11:35:56 +0900

    Feat: success initial test

--- tdd-stack/stack.js
+++ tdd-stack/stack.js
@@ -1,3 +1,26 @@
-class Stack {}
+class Stack {
+  size;
+  stackTop;
+  data;
+  constructor() {
+    this.size = 0;
+    this.stackTop = -1;
+    this.data = [];
+  }
+
+  push(value) {
+    this.stackTop += 1;
+    this.data[this.stackTop] = value;
+    this.size += 1;
+  }
+
+  get top() {
+    return this.data[this.stackTop];
+  }
+
+  pop() {
+    if (this.size === 0) throw new Error('stack is empty');
+  }
+}
 
 module.exports = Stack;
